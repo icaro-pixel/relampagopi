38f5e26a405e3e3cfc17d02f806f1366d8cab015
icaro-pixel <anvil-peso-staff@duck.com>
2026-05-04 14:32:15 -0300

    novo?

--- src/main/java/AlunoEnsinoMedio.java
+++
@@ -1,7 +0,0 @@
-public class AlunoEnsinoMedio extends Aluno{
-
-    @Override
-    public boolean foiAprovado() {
-        return ((getNota1() + getNota2()) / 2) >= 6;
-    }
-}
--- src/main/java/AlunoGraduacao.java
+++
@@ -1,8 +0,0 @@
-public class AlunoGraduacao extends Aluno{
-
-    @Override
-    public boolean foiAprovado() {
-        return ((getNota1() + getNota2()) / 2) >= 7;
-    }
-
-}
